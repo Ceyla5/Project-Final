86f76c0a7545386f8a979c2e334aba3554323bce
User <jeylasg-bf206@gmail.com>
2025-06-30 14:05:54 +0400

    Routes have been created

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,12 +1,32 @@
 
 import './App.css'
+import MainLayout from './Layout/MainLayout'
+import Add from './pages/Add/Add'
+import Admin from './pages/Admin/Admin'
+import Basket from './pages/Basket/Basket'
+import Detail from './pages/Detail/Detail'
+import Home from './pages/Home/Home'
+import NoPage from './pages/NoPage/NoPage'
+import Wishlist from './pages/Wishlist/Wishlist'
 
 function App() {
 
 
   return (
     <>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="add" element={<Add />} />
+          <Route path="detail/:id" element={<Detail />} />
+          <Route path="wishlist" element={<Wishlist />} />
+          <Route path="basket" element={<Basket />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
