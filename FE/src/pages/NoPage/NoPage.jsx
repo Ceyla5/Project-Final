1c90e7ca85febf03cb97294d6b8f161c2f315cd1
User <jeylasg-bf206@gmail.com>
2025-06-30 16:03:15 +0400

    Writing NoPage

--- FE/src/pages/NoPage/NoPage.jsx
+++ FE/src/pages/NoPage/NoPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './style.css'
+
 
 function NoPage() {
   return (
-    <div>NoPage</div>
+    <>
+      <div className="not-found">
+        <h1 className="fade-in">404</h1>
+        <p className="slide-up">Səhifə tapılmadı 😢</p>
+        <a href="/" className="home-link">Ana səhifəyə qayıt</a>
+      </div>
+    </>
   )
 }
 
