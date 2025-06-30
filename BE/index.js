c3d8b7697bf2a5b002ddb2a60b74bc0bb929c985
User <jeylasg-bf206@gmail.com>
2025-06-30 13:57:22 +0400

    Pages have been created

--- BE/index.js
+++ BE/index.js
@@ -1,13 +1,16 @@
 import express from "express"
 import TravelRouter from "./routers/TravelRoutes.js"
 import {authRouter} from "./routers/AuthRouter.js" 
+import cors from "cors"
 import './config/config.js'
 import 'dotenv/config'
+
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 app.use("/travels", TravelRouter)
 app.use("/api/auth", authRouter) 
 
