b0ae0c44865b9257636db4a14fdbd5ba7cc60bbb
User <jeylasg-bf206@gmail.com>
2025-07-03 00:23:40 +0400

    Writing a wishlist page

--- FE/src/pages/Wishlist/Wishlist.jsx
+++ FE/src/pages/Wishlist/Wishlist.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { WishlistContext } from '../../Context/WishlistProvider'
+import "../../pages/Home/style.css"
 
 function Wishlist() {
+  const { wishlist, handleWishlist, checkAtWishlist } = useContext(WishlistContext)
   return (
-    <div>Wishlist</div>
+    <>
+
+      <title>Wishlist</title>
+
+      <div className='cards'>
+        {
+          wishlist.map((x) =>
+            <div key={x._id} className='card'>
+              <div className='cardImg'> <img src={x.image} alt="" /></div>
+              <div className='cardTitle'>
+                <h4>{x.name}</h4>
+                <p>${x.price}</p>
+              </div>
+              <div>
+                <button onClick={() => handleWishlist(x)}> wishlist {checkAtWishlist(x) ? "♥️" : '♡'}</button>
+              </div>
+            </div>)
+        }
+
+      </div>
+    </>
   )
 }
 
