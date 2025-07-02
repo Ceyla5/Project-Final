062f2728a5a71ff045c67be8d32be059d930a494
User <jeylasg-bf206@gmail.com>
2025-07-03 01:01:36 +0400

    Change the Detail page

--- FE/src/pages/Detail/Detail.jsx
+++ FE/src/pages/Detail/Detail.jsx
@@ -9,22 +9,20 @@ function Detail() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/' + id)
+      .get('http://localhost:3000/travels/'+ id)
       .then(res => setdetail(res.data))
   }, [id])
 
   return (
     <>
-
       <title>Detail</title>
-
 
       <div className='cards'>
         <div key={detail._id} className='card'>
-          <div className='cardImg'><img src={detail.img} alt="" /></div>
+          <div className='cardImg'><img src={detail.image} alt="" /></div>
           <div className='cardTitle'>
             <h4>{detail.name}</h4>
-            <p>${detail?.price?.toFixed(2)}</p>
+            <p>{detail?.price?.toFixed(2)} Manat</p>
           </div>
 
         </div>
